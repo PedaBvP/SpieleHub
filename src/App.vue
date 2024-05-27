2ef6568ceed9a7b0a4b254147ff1fe5309da76cc
PedaBvP <84964498+PedaBvP@users.noreply.github.com>
2024-05-27 17:34:31 +0200

    Anpassungen der Fenster größe

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,54 @@
+<!-- src/App.vue -->
 <template>
-  <v-app>
+  <v-layout>
+    <v-app-bar 
+        color="teal-darken-4" image="https://picsum.photos/1920/1080?random">
+        <template v-slot:image>
+          <v-img
+            gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title style="cursor: pointer" @click="$router.push('/')" text="SpieleHub">
+        </v-app-bar-title
+      >
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
     <v-main>
-      <v-container>
-        <router-view></router-view>
-      </v-container>
+        
+        <router-view />
     </v-main>
-  </v-app>
+  </v-layout>
 </template>
 
 <script>
 export default {
   name: 'App',
-}
+};
 </script>
 
 <style>
-/* Vuetify styles werden automatisch importiert */
+
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,19 +1,64 @@
+<!-- src/components/Home.vue -->
 <template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <v-btn color="primary" @click="$router.push('/about')">Go to About</v-btn>
-      </v-container>
-    </v-main>
-  </v-app>
+  <v-card><v-btn color="100 255 100" plain @click="$router.push('/about')"
+          >SpieleHub</v-btn
+        >
+    <v-card-title>
+      <h1 class="display-1">Willkommen zu unserer Spiele-App!</h1>
+    </v-card-title>
+    <v-card-text>
+      <p class="headline">Wählen Sie ein Spiel aus:</p>
+      <v-list>
+        <v-list-item
+          v-for="game in games"
+          :key="game.id"
+          @click="selectGame(game)"
+          button
+          class="ma-2"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ game.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  data() {
+    return {
+      games: [
+        { id: 1, name: "Spiel 1" },
+        { id: 2, name: "Spiel 2" },
+        { id: 3, name: "Spiel 3" },
+        { id: 4, name: "Spiel 4" },
+        { id: 5, name: "Spiel 5" },
+        { id: 6, name: "Spiel 6" },
+        { id: 7, name: "Spiel 7" },
+        { id: 8, name: "Spiel 8" },
+        { id: 9, name: "Spiel 9" },
+        { id: 10, name: "Spiel 10" },
+        { id: 6, name: "Spiel 6" },
+        { id: 7, name: "Spiel 7" },
+        { id: 8, name: "Spiel 8" },
+        { id: 9, name: "Spiel 9" },
+        { id: 10, name: "Spiel 10" },
+      ],
+    };
+  },
+  methods: {
+    selectGame(game) {
+      this.$emit("game-selected", game);
+      alert(`Du hast ${game.name} ausgewählt!`);
+    },
+  },
+};
 </script>
 
-<style>
-/* Vuetify styles werden automatisch importiert */
+<style scoped>
+.ma-2 {
+  margin: 8px;
+}
 </style>
